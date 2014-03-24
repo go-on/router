bdafa394737064f77ecbfae3a450f0ab51e55147
Marc Rene Arns <arns@stridor.net>
2014-03-24 22:08:08 +0100

    add event

---
+++ event/example/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	event "github.com/go-on/router/event"
+)
+
+var (
+	UserGetMail = event.New("user.GetMail")
+)
+
+func EMailAddress(ev event.Event) {
+	name := ev.Param().(string)
+	ev.Return(name + "@example.com")
+}
+
+func init() {
+	UserGetMail.Handle(EMailAddress)
+}
+
+func printEMail(rw http.ResponseWriter, req *http.Request) {
+	ev := UserGetMail.Throw(req.URL.Query().Get("name"))
+	email := ev.Returned()
+	fmt.Fprintf(rw, "email is: %s", email.(string))
+
+}
+
+func main() {
+	event.Watch()
+	http.ListenAndServe(":8081", http.HandlerFunc(printEMail))
+}
